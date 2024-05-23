e1eb5e5c9c048595f1f9722fbdfb24632c809a7e
Vidianos-Giannitsis <vidianosgiannitsis@gmail.com>
2024-05-23 17:41:08 +0300

    Minor additions to some plots from my notebooks

--- scripts/hplc_analysis_28_11.jl
+++ scripts/hplc_analysis_28_11.jl
@@ -17,45 +17,54 @@ df8_conc = process_area_data(get_area_csv(date, mix_amount[5]), get_conc_csv(dat
 
 plot_type = "scatter"
 plot_label = ["0 ml" "1 ml" "2 ml" "4 ml" "8 ml"]
+colors = ["#009AFA" "#E36F47" "#3EA44E" "#C371D2" "#AC8E18"]
 
 suc_scatter = scatter(t, [df0_conc.Sucrose df1_conc.Sucrose df2_conc.Sucrose df4_conc.Sucrose df8_conc.Sucrose], label = plot_label,
 		    xticks = t, title = "Sucrose Concentration",
 		    xlabel = "Time (h)", ylabel = "Sucrose (g/l)", markersize = 6)
+plot!(t, [df0_conc.Sucrose df1_conc.Sucrose df2_conc.Sucrose df4_conc.Sucrose df8_conc.Sucrose], label = false, linecolor = colors)
 savefig(suc_scatter, get_plot_name("sucrose", date, plot_type))
 
 gluc_scatter = scatter(t, [df0_conc.Glucose df1_conc.Glucose df2_conc.Glucose df4_conc.Glucose df8_conc.Glucose], label = plot_label,
 		       xticks = t, title = "Glucose Concentration",
 		       xlabel = "Time (h)", ylabel = "Glucose (g/l)", markersize = 6)
+plot!(t, [df0_conc.Glucose df1_conc.Glucose df2_conc.Glucose df4_conc.Glucose df8_conc.Glucose], label = false, linecolor = colors)
 savefig(gluc_scatter, get_plot_name("glucose", date, plot_type))
 
 fruc_scatter = scatter(t, [df0_conc.Fructose df1_conc.Fructose df2_conc.Fructose df4_conc.Fructose df8_conc.Fructose], label = plot_label,
 		       xticks = t, title = "Fructose Concentration",
 		       xlabel = "Time (h)", ylabel = "Fructose (g/l)", markersize = 6)
+plot!(t, [df0_conc.Fructose df1_conc.Fructose df2_conc.Fructose df4_conc.Fructose df8_conc.Fructose], label = false, linecolor = colors)
 savefig(fruc_scatter, get_plot_name("fructose", date, plot_type))
 
 lact_scatter = scatter(t, [df0_conc.Lactate df1_conc.Lactate df2_conc.Lactate df4_conc.Lactate df8_conc.Lactate], label = plot_label,
 		       xticks = t, title = "Lactate Concentration",
 		       xlabel = "Time (h)", ylabel = "Lactate (g/l)", markersize = 6)
+plot!(t, [df0_conc.Lactate df1_conc.Lactate df2_conc.Lactate df4_conc.Lactate df8_conc.Lactate], label = false, linecolor = colors)
 savefig(lact_scatter, get_plot_name("lactate", date, plot_type))
 
 ac_scatter = scatter(t, [df0_conc.Acetate df1_conc.Acetate df2_conc.Acetate df4_conc.Acetate df8_conc.Acetate], label = plot_label,
 		     xticks = t, title = "Acetate Concentration",
 		     xlabel = "Time (h)", ylabel = "Acetate (g/l)", markersize = 6)
+plot!(t, [df0_conc.Acetate df1_conc.Acetate df2_conc.Acetate df4_conc.Acetate df8_conc.Acetate], label = false, linecolor = colors)
 savefig(ac_scatter, get_plot_name("acetate", date, plot_type))
 
 prop_scatter = scatter(t, [df0_conc.Propionate df1_conc.Propionate df2_conc.Propionate df4_conc.Propionate df8_conc.Propionate], label = plot_label,
 		       xticks = t, title = "Propionate Concentration",
 		       xlabel = "Time (h)", ylabel = "Propionate (g/l)", markersize = 6)
+plot!(t, [df0_conc.Propionate df1_conc.Propionate df2_conc.Propionate df4_conc.Propionate df8_conc.Propionate], label = false, linecolor = colors)
 savefig(prop_scatter, get_plot_name("propionate", date, plot_type))
 
 eth_scatter = scatter(t, [df0_conc.Ethanol df1_conc.Ethanol df2_conc.Ethanol df4_conc.Ethanol df8_conc.Ethanol], label = plot_label,
 		      xticks = t, title = "Ethanol Concentration",
 		      xlabel = "Time (h)", ylabel = "Ethanol (g/l)", markersize = 6)
+plot!(t, [df0_conc.Ethanol df1_conc.Ethanol df2_conc.Ethanol df4_conc.Ethanol df8_conc.Ethanol], label = false, linecolor = colors)
 savefig(eth_scatter, get_plot_name("ethanol", date, plot_type))
 
 pH_scatter = scatter(t, [df0_conc.pH df1_conc.pH df2_conc.pH df4_conc.pH df8_conc.pH], label = plot_label,
 		     xticks = t, title = "pH-t",
 		     xlabel = "Time (h)", ylabel = "pH", markersize = 6, ylims = (3.5, 4.5))
+plot!(t, [df0_conc.pH df1_conc.pH df2_conc.pH df4_conc.pH df8_conc.pH], label = false, linecolor = colors)
 savefig(pH_scatter, get_plot_name("pH", date, plot_type))
 
 scatter_final = plot(suc_scatter, gluc_scatter, fruc_scatter,
